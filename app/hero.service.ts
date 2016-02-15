9d767d538133e61ba4f7da1f7db273409437d25c
joshr <jreuben111@gmail.com>
2016-02-15 06:53:52 +0200

    promise, setTimeout to simulate long running fetch

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,9 +1,12 @@
 import {Injectable} from 'angular2/core';
 import {HEROES} from './mock-heroes';
+import {Hero} from './hero';
 
 @Injectable()
 export class HeroService {
   getHeroes() {
-    return HEROES;
+    return new Promise<Hero[]>(resolve =>
+    setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
+  );
   }
 }
